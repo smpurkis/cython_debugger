32600c3a8b0b79c051f6adabe4a9b2969ba0d1c7
Sam Purkis <sam.purkis@hotmail.co.uk>
2021-04-17 15:33:34 +0100

    project folder bug

--- cygdb_server/Cygdb_server.py
+++ cygdb_server/Cygdb_server.py
@@ -139,10 +139,9 @@ class CythonServer:
 
     def setup_files(self):
         if self.debug_path and self.python_debug_executable_path:
+            output, successful_compile = cythonize_files(self.python_debug_executable_path)
             self.gdb_configuration_file = make_command_file(self.debug_path)
-            return cythonize_files(
-                self.python_debug_executable_path
-            )
+            return output, successful_compile
 
     def format_progress(self, resp):
         if len(resp) == 0:
