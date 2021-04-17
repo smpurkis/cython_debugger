381513baebfc3a6d112a91562627d8981793821c
Sam Purkis <sam.purkis@hotmail.co.uk>
2021-04-17 23:32:08 +0100

    project folder bug

--- cygdb_server/Cygdb_server.py
+++ cygdb_server/Cygdb_server.py
@@ -43,13 +43,17 @@ def copy_mounted_folder_to_working_folder():
     print(f"ls -laths {WORKING_FOLDER}/project_folder")
     sp.call(f"ls -laths {WORKING_FOLDER}/project_folder", shell=True)
 
-    cmd = f"mv /project_folder {WORKING_FOLDER}"
+    cmd = f"mv .{WORKING_FOLDER}/project_folder {WORKING_FOLDER}"
     print(cmd)
     sp.call(cmd.split())
+
     print(f"ls -laths {WORKING_FOLDER}")
     sp.call(f"ls -laths {WORKING_FOLDER}", shell=True)
     print(f"ls -laths {WORKING_FOLDER}/project_folder")
     sp.call(f"ls -laths {WORKING_FOLDER}/project_folder", shell=True)
+
+    print(f"pwd")
+    sp.call(f"pwd", shell=True)
 
 
 copy_mounted_folder_to_working_folder()
